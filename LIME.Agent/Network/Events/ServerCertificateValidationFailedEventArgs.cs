72efb0934a5a16aea1697f6234ba77ac5e8e8d28
lewpar <lewis.o.parmenter@gmail.com>
2024-09-05 19:51:22 +1000

    Added a LimeClient class to separate network responsibility from the agent.

---
+++ LIME.Agent/Network/Events/ServerCertificateValidationFailedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Security;
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace LIME.Agent.Network.Events;
+
+internal class ServerCertificateValidationFailedEventArgs
+{
+    public X509Certificate? Certificate { get; set; }
+    public X509Chain? Chain { get; set; }
+
+    public SslPolicyErrors SslPolicyErrors { get; set; }
+
+    public ServerCertificateValidationFailedEventArgs(X509Certificate? certificate, X509Chain? chain, SslPolicyErrors sslPolicyErrors)
+    {
+        Certificate = certificate;
+        Chain = chain;
+
+        SslPolicyErrors = sslPolicyErrors;
+    }
+}
--- LIME.Agent/Services/LimeAgent.cs
+++ LIME.Agent/Services/LimeAgent.cs
@@ -1,44 +1,65 @@
 ﻿using LIME.Agent.Configuration;
-using LIME.Shared.Crypto;
+using LIME.Agent.Network;
+
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-using System.Net.Security;
-using System.Net.Sockets;
+using System.Net;
 
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace LIME.Agent.Services;
 
 internal partial class LimeAgent : IHostedService
 {
+    private LimeClient client;
+
     private readonly ILogger<LimeAgent> logger;
     private readonly LimeAgentConfig config;
 
-    private X509Certificate2 certificate;
-
-    private TcpClient client;
-
     public LimeAgent(ILogger<LimeAgent> logger, LimeAgentConfig config)
     {
+        client = new LimeClient();
+        client.LoadClientCertificate(config.Certificate.Thumbprint);
+
+        client.ClientConnected += Client_ClientConnected;
+        client.ServerCertificateValidationFailed += Client_ServerCertificateValidationFailed;
+
         this.logger = logger;
         this.config = config;
-
-        certificate = GetCertificate(config.Certificate.Thumbprint);
-
-        client = new TcpClient();
     }
 
-    private X509Certificate2 GetCertificate(string certificateThumbprint)
+    private void Client_ClientConnected(object? sender, EventArgs e)
     {
-        var cert = LimeCertificate.GetCertificate(certificateThumbprint);
-        if (cert is null)
+        logger.LogInformation("Connected to mediator server.");
+    }
+
+    private void Client_ServerCertificateValidationFailed(object? sender, Network.Events.ServerCertificateValidationFailedEventArgs e)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine($"Got ssl policy error: {e.SslPolicyErrors.ToString()}");
+
+        if (e.Chain is null)
         {
-            throw new NullReferenceException($"No certificate was found with the thumbprint '{certificateThumbprint}'.");
+            sb.AppendLine("Chain is null, could not retrieve status.");
+            logger.LogCritical(sb.ToString());
+            return;
         }
 
-        return cert;
+        foreach (var item in e.Chain.ChainElements)
+        {
+            if (item.ChainElementStatus.Length > 0)
+            {
+                sb.AppendLine($"   {item.Certificate.Subject}");
+            }
+
+            foreach (var status in item.ChainElementStatus)
+            {
+                sb.AppendLine($"       {status.Status}: {status.StatusInformation}");
+            }
+        }
+
+        logger.LogCritical(sb.ToString());
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
@@ -55,23 +76,13 @@ internal partial class LimeAgent : IHostedService
     {
         try
         {
-            await client.ConnectAsync(config.MediatorAddress, config.MediatorPort);
-
-            var stream = new SslStream(client.GetStream(), false, ValidateServerCertificate);
-
-            await stream.AuthenticateAsClientAsync(new SslClientAuthenticationOptions()
+            if(!IPAddress.TryParse(config.MediatorAddress, out IPAddress? address))
             {
-                ClientCertificates = new X509CertificateCollection()
-                {
-                    certificate
-                },
-                CertificateRevocationCheckMode = X509RevocationMode.Online,
-                CertificateChainPolicy = new X509ChainPolicy()
-                {
-                    RevocationFlag = X509RevocationFlag.EntireChain,
-                },
-                TargetHost = config.MediatorHost
-            });
+                logger.LogCritical("Failed to parse IP address for mediator.");
+                return false;
+            }
+
+            await client.ConnectAsync(config.MediatorHost, new IPEndPoint(address, config.MediatorPort));
         }
         catch(Exception ex)
         {
@@ -80,45 +91,6 @@ internal partial class LimeAgent : IHostedService
         }
 
         return true;
-    }
-
-    private bool ValidateServerCertificate(object sender, X509Certificate? certificate, X509Chain? chain, SslPolicyErrors sslPolicyErrors)
-    {
-        if(chain is null || certificate is null)
-        {
-            return false;
-        }
-
-        if(sslPolicyErrors == SslPolicyErrors.None)
-        {
-            return true;
-        }
-
-        var sb = new StringBuilder();
-        sb.AppendLine($"Got ssl policy error: {sslPolicyErrors.ToString()}");
-
-        if (chain is null)
-        {
-            sb.AppendLine("Chain is null, could not retrieve status.");
-            return false;
-        }
-
-        foreach (var item in chain.ChainElements)
-        {
-            if(item.ChainElementStatus.Length > 0)
-            {
-                sb.AppendLine($"   {item.Certificate.Subject}");
-            }
-
-            foreach (var status in item.ChainElementStatus)
-            {
-                sb.AppendLine($"       {status.Status}: {status.StatusInformation}");
-            }
-        }
-
-        logger.LogCritical(sb.ToString());
-
-        return false;
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
